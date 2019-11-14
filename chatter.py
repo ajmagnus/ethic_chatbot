1ddd138366cfd0227c18f33116e6fd6a0b101733
Glen Halonen <ghalonen@c17-red-06.ad.mtu.edu>
2019-11-14 16:57:28 -0500

    Chatbot can hold a conversation

--- chatter.py
+++ chatter.py
@@ -1,13 +1,16 @@
 from chatterbot import ChatBot
 def main():
-	print("Python main func");
 	from chatterbot import ChatBot
 	chatbot = ChatBot("Trump");
-	response = chatbot.get_response("Good morn");
-	print(response);
+	msg = 'hello!';
+	print(msg);
+	while msg != 'bye':
+		msg = input(':');
+		print(msg);
+		#response = chatbot.get_response(msg);
+		#print(response);
 
-
-
+	print('bye!');
 
 if __name__ == '__main__':
 	main()
