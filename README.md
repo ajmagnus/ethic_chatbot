d431ba2f52f64ca18f93765e829d8a2c523d4008
Glen Halonen <ghalonen@c28-0113-05.ad.mtu.edu>
2019-11-16 21:11:37 -0500

    Chatbot uses trump dir

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ git clone https://github.com/ajmagnus/ethic_chatbot.git
 git add .
 git commit -m "message"
 git push origin master
+
+For adding a new.yml file to be used by chatter bot
+Go to this location:
+/home/campus08/<user>/.local/lib/python3.6/site-packages/chatterbot_corpus/data/trump
+and add the file
+
+The file should now be used by chatterbot.
+
+Please commit your .yml to github when ready to use.
--- chatter.py
+++ chatter.py
@@ -1,16 +1,42 @@
 from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import chatterbot
 def main():
 	from chatterbot import ChatBot
-	chatbot = ChatBot("Trump");
-	msg = 'hello!';
+	chatbot = ChatBot(
+			"Trump", 
+			read_only=False,
+			#logic_adapters=[
+			#	{
+			#		"response_selection_method": chatterbot.response_selection.get_random_response
+			#	}
+			#]
+	)
+		
+	#train the chatbot
+	trainer = ChatterBotCorpusTrainer(chatbot)
+	#trainer.train(
+	#	"convo.yml"
+	#)
+
+	trainer.train(
+	    "chatterbot.corpus.trump"
+	)
+
+		
+
+	chatbot.read_only=True
+	
+	#have conversation
+	msg = 'hello!'
 	print(msg);
 	while msg != 'bye':
-		msg = input(':');
-		print(msg);
-		#response = chatbot.get_response(msg);
-		#print(response);
+		msg = input(':')
+		#print(msg)
+		response = chatbot.get_response(msg)
+		print(response)
 
-	print('bye!');
+	print('bye!')
 
 if __name__ == '__main__':
 	main()
